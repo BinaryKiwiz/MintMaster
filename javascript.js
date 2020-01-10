506c257a6b429e8b98e639cde3c44534dfea3d5b
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-01-09 19:10:56 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
+var Mints = 0;
+var MintsPerClick = 1;
+var Money = 0;
+var MoneyPerMint = 0.1;
 
+function ManufactureMint(){
+  Mints = Mints + MintsPerClick;
+  document.getElementById("Mints").innerHTML = Mints;
+}
