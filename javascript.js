e63afe59d366707b2b6bae0bdf50aaeb66729b83
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-01-09 19:45:16 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,3 +7,11 @@ function ManufactureMint(){
   Mints = Mints + MintsPerClick;
   document.getElementById("Mints").innerHTML = Mints;
 }
+
+function SellMints(){
+  Money = Money + Mints * MoneyPerMint
+  Money = Math.round(Money*10) / 10;
+  Mints = 0
+  document.getElementById("Money").innerHTML = Money;
+  document.getElementById("Mints").innerHTML = Mints;
+}
