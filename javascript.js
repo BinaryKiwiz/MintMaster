e994cf5a3455e2a762e7ed6eb47220be63fc8814
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-01-09 21:30:08 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,21 @@ var Mints = 0;
 var MintsPerClick = 1;
 var Money = 0;
 var MoneyPerMint = 0.1;
+var MPS = 0;
+
+function ImportSave(){
+  if(localStorage.getItem('Mints')){
+    Mints = localStorage.getItem('Mints');
+  }
+
+  if(localStorage.getItem('Money')){
+    Money = localStorage.getItem('Money');
+  }
+  document.getElementById("Mints").innerHTML = Mints;
+  document.getElementById("Money").innerHTML = Money;
+  Mints = parseInt(Mints);
+  Money = parseInt(Money);
+}
 
 function ManufactureMint(){
   Mints = Mints + MintsPerClick;
@@ -14,4 +29,11 @@ function SellMints(){
   Mints = 0
   document.getElementById("Money").innerHTML = Money;
   document.getElementById("Mints").innerHTML = Mints;
+}
+
+function Save(){
+  localStorage.setItem('Mints', Mints);
+  localStorage.setItem('Money', Money);
+  document.getElementById("Mints").innerHTML = Mints;
+document.getElementById("Money").innerHTML = Money;
 }
