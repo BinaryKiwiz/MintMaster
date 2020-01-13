9fd647662e460e28ee8ad01e4333f8bd06a35a7a
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-01-13 13:10:21 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,25 +5,32 @@ var Money = 0;
 var MoneyPerMint = 0.01;
 var MPS = 0;
 var NumMV = 0;
-var CostMV = 25;
+var CostMV = 5;
 var MPSMV = 1;
 var Clicks = 0;
 var InfoBoxInfo = "Bruh";
 var x = "0";
 var InfoBoxMoney = 0;
+var EffManufacturingPrice = 50;
 
 //Game Loading and Screen Refresh
 document.getElementById("Mints").innerHTML = Mints;
 document.getElementById("Money").innerHTML = Money;
 document.getElementById("InfoBoxInfo").innerHTML = InfoBoxInfo;
-document.getElementById("ManditoryVolunteerPrice").innerHTML = CostMV;
+document.getElementById("MandatoryVolunteerPrice").innerHTML = CostMV;
+document.getElementById("MPS").innerHTML = MPS;
+document.getElementById("EfficientManufacturingPrice").innerHTML = EffManufacturingPrice;
 
 //Functions
 function RefreshScreen(){
   document.getElementById("Mints").innerHTML = Mints;
   document.getElementById("Money").innerHTML = Money;
   document.getElementById("InfoBoxInfo").innerHTML = InfoBoxInfo;
-  document.getElementById("ManditoryVolunteerPrice").innerHTML = CostMV;
+  Money = Math.round(Money*100) / 100;
+  document.getElementById("MandatoryVolunteerPrice").innerHTML = CostMV;
+  document.getElementById("MPS").innerHTML = MPS;
+  
+  document.getElementById("EfficientManufacturingPrice").innerHTML = EffManufacturingPrice;
 }
 function ImportSave(){
   if(localStorage.getItem('Mints')){
@@ -45,7 +52,10 @@ function ImportSave(){
 function ManufactureMint(){
   Mints = Mints + MintsPerClick;
   Clicks = Clicks + 1;
-  document.getElementById("Mints").innerHTML = Mints;
+  if(InfoBoxInfo != "You Manufactued a Mint"){
+    InfoBoxInfo = "You Manufactured a Mint";
+  }
+  RefreshScreen();
 }
 
 function SellMints(){
@@ -54,7 +64,7 @@ function SellMints(){
   InfoBoxInfo = "You Sold all of your Mints for $" + InfoBoxMoney;
   Money = Money + Mints * MoneyPerMint;
   Money = Math.round(Money*100) / 100;
-  Mints = 0;;
+  Mints = 0;
   RefreshScreen();
 }
 
@@ -62,11 +72,38 @@ function Save(){
   localStorage.setItem('Mints', Mints);
   localStorage.setItem('Money', Money);
   localStorage.setItem('Clicks', Clicks);
+  InfoBoxInfo = "Game Saved";
+  RefreshScreen();
 }
 
-function BuyManditoryVolunteer(){
-  if (Money >= 25){
-    Money = Money - 25;
+function BuyMandatoryVolunteer(){
+  if (Money >= CostMV){
+    Money = Math.round((Money - CostMV) * 100) / 100;
+    CostMV = Math.round((CostMV * 1.05) * 100) / 100;
+    MPS = MPS + MPSMV;
+    if(InfoBoxInfo != "You went to the auction and you bought a Manditory Volunteer to work 24/7. Oh and you don't need to pay them. Isn't that convenient?"){
+      InfoBoxInfo = "You went to the auction and you bought a Manditory Volunteer to work 24/7. Oh and you don't need to pay them. Isn't that convenient?";
+    }
     RefreshScreen();
   }
 }
+
+function AddMPS(){
+  Mints = Math.round(Mints + MPS);
+  setTimeout(AddMPS, 1000);
+  RefreshScreen();
+}
+
+function ResearchEffManufacturing(){
+  if(Mints >= 50){
+    Mints = Mints - EffManufacturingPrice;
+    Mints = Math.round(Mints);
+    MintsPerClick = MintsPerClick + 1;
+    EffManufacturingPrice = Math.round(EffManufacturingPrice * 1.05);
+    InfoBoxInfo = "You threw some mints at a Manditory Volunteer, and they research some efficiency stuff or something. You feel your hands become MORE POWERFUL";
+    RefreshScreen();
+  }
+}
+
+//Post-Function Random Commands like MPS
+AddMPS();
